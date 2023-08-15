7893d111011495c0be8a2cf2ac48fb1f98f0e701
Ebin Soby <mailebinsoby@gmail.com>
2023-08-14 22:25:14 -0400

    componentalizing

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,14 +1,17 @@
+import Assignment from './Assignment.js'
+
 export default {
     template: `<section v-show="assignments.length">
     <h2 class="font-bold mb-2">{{title}}</h2>
     <ul>
-      <li v-for="assignment in assignments" :key="assignment.id">
-        <label>{{assignment.name}} <input type="checkbox" v-model="assignment.completed"></label>
-      </li>
+      <assignment v-for="assignment in assignments" :assignment=assignment :key="assignment.id"></assignment>
     </ul>
   </section>`,
   props: {
     assignments:Array,
     title:String,
+  },
+  components: {
+    Assignment,
   }
 }
