bcf767f7f6331afaf219048248e5a0124d498e51
esoby <ebin.soby@alertdriving.com>
2023-08-16 16:58:37 -0400

    tags

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,17 +1,34 @@
-import Assignment from './Assignment.js'
+import Assignment from "./Assignment.js";
 
 export default {
-    template: `<section v-show="assignments.length">
-    <h2 class="font-bold mb-2">{{title}}</h2>
-    <ul class="border border-gray-600 divide-y divide-gray-600">
-      <assignment v-for="assignment in assignments" :assignment=assignment :key="assignment.id"></assignment>
+  template: `<section v-show="assignments.length">
+    <h2 class="font-bold mb-2">{{title}}
+    <span>({{assignments.length}})</span></h2>
+    <div class="flex gap-2">
+      <button v-for="tag in tags" class="border rounded px-1 py-px text-xs" @click="()=>{currentTag = tag}">{{tag}}</button>
+    </div>
+    <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+      <assignment v-for="assignment in filteredAssignments" :assignment=assignment :key="assignment.id"></assignment>
     </ul>
   </section>`,
   props: {
-    assignments:Array,
-    title:String,
+    assignments: Array,
+    title: String,
+  },
+  data(){
+    return{
+      currentTag:'',
+    }
   },
   components: {
     Assignment,
+  }, 
+  computed: {
+    tags(){
+      return new Set(this.assignments.map((assignment)=>assignment.tag))
+    },
+    filteredAssignments(){
+      return   this.assignments.filter((assignment)=>assignment.tag === this.currentTag)
+    }
   }
-}
+};
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,27 +1,21 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 export default {
   template: `
     <section class="space-y-6">
     <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
     <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
-    <form @submit.prevent="add" >
-    <div class="border border-gray-600 text-black">
-    <input v-model="newAssignment" placeholder="New Assignment..." class="p-2"/>
-      <button type="submit" class="bg-white p-2 border-l">Add</button>
-    </div>
-      
-    </form>
+    <assignment-create :assignments="assignments" @add="add"></assignment-create>
     </section>
     `,
   data() {
     return {
       assignments: [
-        { name: "Chapter 1", completed: true, id: 1 },
-        { name: "Chapter 2", completed: false, id: 2 },
-        { name: "Home work", completed: false, id: 3 },
-        { name: "Plays", completed: true, id: 4 },
+        { name: "Chapter 1", completed: true, id: 1, tag:'math' },
+        { name: "Chapter 2", completed: false, id: 2, tag:'science' },
+        { name: "Home work", completed: false, id: 3, tag:'math' },
+        { name: "Plays", completed: true, id: 4, tag:'arts' },
       ],
-      newAssignment : '',
     };
   },
   computed: {
@@ -38,15 +32,15 @@ export default {
   },
   components: {
     AssignmentList,
+    AssignmentCreate,
   },
-  methods : {
-    add(){
+  methods: {
+    add(name) {
       this.assignments.push({
-        name:this.newAssignment,
-        completed:false,
-        id:this.assignments.length+1,
-      })
-      this.newAssignment='';
-    }
+        name: name,
+        completed: false,
+        id: this.assignments.length + 1,
+      });
+    },
   },
 };
