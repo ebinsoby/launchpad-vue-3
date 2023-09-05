91bb9fb05015f79848ee12a9f68f272b93a77a1d
esoby <ebin.soby@alertdriving.com>
2023-09-05 17:13:11 -0400

    component responsibility

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,33 +1,30 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from './AssignmentTags.js'
 
 export default {
   template: `<section v-show="assignments.length">
     <h2 class="font-bold mb-2">{{title}}
     <span>({{assignments.length}})</span></h2>
-    <div class="flex gap-2">
-      <button v-for="tag in tags" class="border rounded px-1 py-px text-xs" @click="()=>{currentTag = tag}">{{tag}}</button>
-    </div>
+    <assignment-tags :initialTags="this.assignments.map((assignment)=>assignment.tag)" @change="currentTag = $event" :currentTag="currentTag"/>
     <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
       <assignment v-for="assignment in filteredAssignments" :assignment=assignment :key="assignment.id"></assignment>
     </ul>
   </section>`,
   props: {
-    assignments: Array,
+    assignments: Array, 
     title: String,
   },
   data(){
     return{
-      currentTag:'',
+      currentTag:'all',
     }
   },
   components: {
-    Assignment,
+    Assignment, AssignmentTags,
   }, 
   computed: {
-    tags(){
-      return new Set(this.assignments.map((assignment)=>assignment.tag))
-    },
     filteredAssignments(){
+      if(this.currentTag ==='all')return this.assignments;
       return   this.assignments.filter((assignment)=>assignment.tag === this.currentTag)
     }
   }
